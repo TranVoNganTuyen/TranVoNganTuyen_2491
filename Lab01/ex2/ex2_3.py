6ddd4226071b096a02409460444b666131e95970
unknown <tngan11905@gmail.com>
2026-02-03 15:34:34 +0700

    [add] Lab01 ex01 ex2

---
+++ Lab01/ex2/ex2_3.py
@@ -0,0 +1,8 @@
+#Nhập số từ người dùng 
+so= int (input("Nhập số nguyên:"))
+#Kiểm tra xem số đó có pahir số chẵn hay không
+if so %2 ==0:
+        print(so,"là số chẵn")
+else:
+    print(so,"không phải là số chẵn.")
+    
